19a05adfcea78d6575f2fb22c5f8f4caf770b848
Luke Willson <42656109+swills3@users.noreply.github.com>
2019-04-08 00:33:54 -0500

    Update signup.module.ts

--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEM } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
